79f75920ba68cb6451bb16e6b105b963cdfe1273
saivishnuadabala <20A91A0249@aec.edu.in>
2024-09-06 00:18:11 +0530

    version1

---
+++ script.js
@@ -0,0 +1,12 @@
+var settingsmenu=document.querySelector(".settings-menu")
+
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height")
+}
+
+var hello=document.querySelector(".bar-menu")
+
+function barMenuToggle(){
+    hello.classList.toggle("bar-menu-height")
+}
